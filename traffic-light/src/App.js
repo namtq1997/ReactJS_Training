34c4bc1941537c305519fc13f2fe3d97eeb72ab1
namtq1997 <52656405+namtq1997@users.noreply.github.com>
2019-07-16 11:43:46 +0700

    Bài 12: traffic-light

---
+++ traffic-light/src/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TrafficLight from './components/TrafficLight'
+
+const RED = 0;
+const ORANGE = 1;
+const GREEN = 2;
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        currentColor: ORANGE
+    };
+
+    setInterval(() => {
+        this.setState({
+            currentColor : this.getNextColor(this.state.currentColor)
+        });
+    }, 2000);
+  }
+
+  getNextColor(color) {
+    switch (color) {
+      case RED:
+          return ORANGE;
+      case ORANGE:
+          return GREEN;
+      case GREEN:
+          return RED;
+    }
+  }
+
+  render () {
+    const {currentColor} = this.state;
+    
+    return (
+      <div className="App">
+        <TrafficLight currentColor ={currentColor} />    
+      </div>
+    );
+  }
+}
+
+export default App;
